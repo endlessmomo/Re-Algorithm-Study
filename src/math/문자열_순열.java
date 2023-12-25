d56c531677cd30db26aa28b6353ae9ac5fc95436
endelssmomo <soohyuk96@naver.com>
2023-12-25 23:16:12 +0900

    [수학문제] : 문자열 순회 문제 풀이 2

--- src/math/문자열_순열.java
+++ src/math/문자열_순열.java
@@ -8,6 +8,7 @@ public class 문자열_순열 {
     static boolean[] used;
     static char[] chars;
     static Set<String> strings;
+
     public static boolean sol(String value, String target) {
 
         strings = new HashSet<>();
@@ -19,19 +20,53 @@ public class 문자열_순열 {
         return strings.stream().anyMatch(target::contains);
     }
 
-    public static void dfs(int cnt, int length, String value){
-        if(cnt == length) {
+    public static void dfs(int cnt, int length, String value) {
+        if (cnt == length) {
             strings.add(value);
             return;
         }
 
-        for(int i = 0; i < length; i++){
-            if(!used[i]) {
+        for (int i = 0; i < length; i++) {
+            if (!used[i]) {
                 used[i] = true;
                 dfs(cnt + 1, length, value + chars[i]);
                 used[i] = false;
             }
         }
+    }
+
+    public static boolean sol2(String s1, String s2) {
+        final int ALPHABET = 26;
+
+        if (s1.length() > s2.length()) {
+            return false;
+        }
+
+        int[] cnt = new int[ALPHABET];
+        for (int i = 0; i < s1.length(); i++) {
+            cnt[s1.charAt(i) - 'a']++;
+        }
+
+        for (int i = 0; i < s2.length(); i++) {
+            cnt[s2.charAt(i) - 'a']--;
+
+            if (i - s1.length() >= 0) {
+                cnt[s2.charAt((i - s1.length()) - 'a')]++;
+            }
+
+            boolean isZero = true;
+            for (int j = 0; j < ALPHABET; j++) {
+                if (cnt[j] != 0) {
+                    isZero = false;
+                    break;
+                }
+            }
+
+            if(isZero) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public static void main(String[] args) {

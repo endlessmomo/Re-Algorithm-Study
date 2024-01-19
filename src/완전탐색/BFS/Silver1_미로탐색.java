e14993826f157c93b2e7cc84ebac3b814a573888
endelssmomo <soohyuk96@naver.com>
2024-01-19 19:33:14 +0900

    [완전탐색-BFS] : [Silver-1] 미로 탐색

---
+++ src/완전탐색/BFS/Silver1_미로탐색.java
@@ -0,0 +1,67 @@
+package 완전탐색.BFS;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Silver1_미로탐색 {
+
+  static int[] dX = {1, 0, -1, 0};
+  static int[] dY = {0, 1, 0, -1};
+  static int N;
+  static int M;
+  static int[][] board;
+  static boolean[][] visited;
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(in.readLine());
+
+    N = Integer.parseInt(st.nextToken()); // 행
+    M = Integer.parseInt(st.nextToken()); // 열
+
+    board = new int[N][M];
+    visited = new boolean[N][M];
+    for (int i = 0; i < N; i++) {
+      board[i] = Arrays.stream(in.readLine().split(""))
+          .mapToInt(Integer::parseInt)
+          .toArray();
+    }
+
+    int answer = bfs(0, 0);
+    System.out.println(answer);
+  }
+
+  public static int bfs(int startX, int startY) {
+    Queue<int[]> queue = new LinkedList<>();
+    queue.offer(new int[] {startX, startY});
+
+    while (!queue.isEmpty()) {
+      int[] cur = queue.poll();
+
+      if (cur[0] == N - 1 && cur[1] == M - 1) break;
+
+      for (int i = 0; i < 4; i++) {
+        int dx = cur[0] + dX[i];
+        int dy = cur[1] + dY[i];
+
+        if (dx >= 0 && dy >= 0 && dy < N && dx < M) {
+          if (board[dy][dx] == 0) {
+            continue;
+          }
+
+          if (!visited[dy][dx]) {
+            board[dy][dx] = board[dy][dx] + board[cur[1]][cur[0]];
+            visited[dy][dx] = true;
+            queue.offer(new int[]{dx, dy});
+          }
+        }
+      }
+    }
+    return board[N - 1][M - 1];
+  }
+}
